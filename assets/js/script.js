f0e0bfa63efe43114c60b4536d6b541fd5c817ea
sarebok <bortonae@hotmail.com>
2023-08-29 23:53:33 -0400

    save basic data in array and some console.logs to verify

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,17 @@
 const newTaskName = document.getElementById("new-task-name");
 const taskCardsContainer = document.getElementById("task-cards-container");
 const addTaskButton = document.getElementById("add-task-button");
+//1.1 I think I will need an array to save things
+const tasks = [];
+//2.- Basic Object Structure: [taskName:xxx, Status:[backlog, doing, done]]
 
-//create card
 addTaskButton.addEventListener("click", () => {
+  //create card
   const taskName = newTaskName.value;
-  console.log(taskName);
+  console.log("task name " + taskName);
   const taskCardDiv = document.createElement("div");
+  tasks.push(taskName, "backlog");
+  console.log("array tasks " + tasks + " elements in array: " + tasks.length);
   taskCardDiv.innerHTML = `
     <h1>${taskName}
   `;

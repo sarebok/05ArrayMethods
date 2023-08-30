37a4b1d43f0933cc753c85f04e9e89d76ca62548
sarebok <bortonae@hotmail.com>
2023-08-30 00:13:24 -0400

    fix saving object on tasks array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,27 @@
 const newTaskName = document.getElementById("new-task-name");
 const taskCardsContainer = document.getElementById("task-cards-container");
 const addTaskButton = document.getElementById("add-task-button");
-//1.1 I think I will need an array to save things
-const tasks = [];
+//1.1 Array with the first three tasks
+const tasks = [
+  ["programar tarea del desafio", "backlog"],
+  ["subir desafio a plataforma", "backlog"],
+  ["inscribirme en desafiolatam", "done"],
+];
 //2.- Basic Object Structure: [taskName:xxx, Status:[backlog, doing, done]]
 
 addTaskButton.addEventListener("click", () => {
   //create card
+  createTaskCard();
+});
+
+function createTaskCard() {
   const taskName = newTaskName.value;
   console.log("task name " + taskName);
   const taskCardDiv = document.createElement("div");
-  tasks.push(taskName, "backlog");
-  console.log("array tasks " + tasks + " elements in array: " + tasks.length);
+  tasks.push([taskName, "backlog"]);
+  console.log("array tasks: " + tasks + " elements in array: " + tasks.length);
   taskCardDiv.innerHTML = `
-    <h1>${taskName}
-  `;
+      <h1>${taskName}
+    `;
   taskCardsContainer.appendChild(taskCardDiv);
-});
+}

b040662e7b988e03eff830c1607c9123b16bbb04
sarebok <bortonae@hotmail.com>
2023-09-02 19:13:55 -0400

    working now with objects and not array elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,48 @@
 const newTaskNameInput = document.getElementById("new-task-name");
 const taskCardsContainer = document.getElementById("task-cards-container");
 const addTaskButton = document.getElementById("add-task-button");
+//1.1 main object structure
+
+const taskObject = { id: "", name: "", status: "backlog" };
+
 //1.1 Array with the first three tasks
 const tasks = [
   ["programar tarea del desafio", "backlog"],
   ["subir desafio a plataforma", "backlog"],
   ["inscribirme en desafiolatam", "done"],
 ];
-//2.- Basic Object Structure: [taskName:xxx, Status:[backlog, doing, done]]
 
-tasks.forEach(createTaskCard);
+//now I am gonna try to set an array of the first 3 taskObjects
+const tasks2 = [
+  { id: "", name: "programar tarea del desafio", status: "backlog" },
+  { id: "", name: "subir desafio a plataforma", status: "backlog" },
+  { id: "", name: "inscribirme en desafiolatam", status: "backlog" },
+];
+
+//the game begins
+tasks2.forEach(createTaskCard);
 
 addTaskButton.addEventListener("click", () => {
   taskCardsContainer.innerHTML = "";
   //insert new object to array
   const taskName = newTaskNameInput.value;
-  tasks.unshift([taskName, "backlog"]);
+  tasks2.unshift({ id: Date.now(), name: taskName, status: "backlog" });
   //create card
-  tasks.forEach(createTaskCard);
-  console.log(tasks);
+  tasks2.forEach((task) => {
+    task.id = Date.now();
+    createTaskCard(task);
+  });
+  console.log(tasks2);
 });
 
 function createTaskCard(task) {
   const taskCardDiv = document.createElement("div");
   taskCardDiv.classList.add("task-card-div");
   taskCardDiv.innerHTML = `
-      <h1>${task}
+      <h1>${task.name}
       <input type="checkbox" unchecked>
+      <button>Doing</button>
+      <button>Done</button>
     `;
   taskCardsContainer.appendChild(taskCardDiv);
 }
